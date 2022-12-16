0d35b8dd11e9b4c422a0ca9aa4b8d0f2c21816bc
KirisakiXOnodera <72533293+KirisakiXOnodera@users.noreply.github.com>
2022-12-16 09:20:34 +0800

    file extension checker

--- src/main.c
+++ src/main.c
@@ -7,8 +7,25 @@
 
 int main()
 {
+    char filepath[100];
+    int len;
+    printf("Input file path: ");
+    scanf("%s",filepath);
+    len = strlen(filepath);
+    if (len < 1) {
+        printf("Invalid file path");
+        return 0;
+    }
+    if(filepath[len-1] != 'c' && filepath[len-2] != '.'){
+        printf("Invalid file extension\n.\n.\nProgram will now close");
+        return 0;
+    }else{
+        inputptr = fopen(filepath, "r");
+    }
+
+
     char contents [1000];
-    inputptr = fopen("examples/example.c", "r");
+
     outputptr = fopen("examples/SymbolTable.txt","w");
     if (inputptr == NULL){
         perror("Unable to open the file");
