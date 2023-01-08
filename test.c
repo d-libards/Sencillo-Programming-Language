5a6ebbad9474edbf0cf28ea5c6989a213a0d7be2
KirisakiXOnodera <72533293+KirisakiXOnodera@users.noreply.github.com>
2023-01-08 16:31:36 +0800

    -m "comment test"

--- test.c
+++ test.c
@@ -4,7 +4,8 @@ int main()
 {
     printf("Name\t:\tAlexandta Abramov\n");
     printf("DOB\t\t:\tJuly 14, 1975\n");
-    printf("Mobile\t:\t99-999999999");
+    printf("Mobile\t:\t99-999999999\n");
+    printf("Dominique Libardo pogi");
 
     return 0;
 }
