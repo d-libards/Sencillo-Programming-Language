b16f4a6901ce545543de7e13951e093de6c22ff0
KirisakiXOnodera <72533293+KirisakiXOnodera@users.noreply.github.com>
2023-01-12 23:22:02 +0800

    rename lexicalanalyzer

--- sample.s
+++ sample.s
@@ -1,10 +1,95 @@
+name&
+name_
+na_me
+#name
+$name
+#name
+_nume
+12name
+na12me
+name12
+domskrt
+1
+12.3
+1.2.3
+er
+"\"no\""
+"\\backward slash\\"
+"\"\""
+"\""
+"\\"
+"efren \"bata\" reyes"
+''
+' '
+'\''
+'\\'
+'"'
+'hello'
+'h'
+'\'hello\''
+'ss\'s\''
+'\\h\\'
+'holla\\\''
+/*Identifier*/
+dom
+my_name
+name14
+/*Assignment Operators*/
+= += -=  *=  /=  %=  //= 
+/*Unary Operators*/
+++ -- 
+/*Arithmetic Operators*/
++ - * / % ** //
+/*Logical Operators*/
+y ni no
+/*Boolean Relational Operators*/ 
+== != > < >= <=
+/*Keywords*/
+bool cuer dec descanso doble ent escan flot hacer impri mas mientras ni no perso por retorno sequir si vacio vamos y
+/*Reserved Word*/
+falso verdad
+/*Noise Words*/
+booleano decontra personaje entero escanear flotar cuerda imprimir
+/*Delimeters*/
+: () [ ] { } , ;
+/*invalid*/
+@
+_num
+*/
+'invalid'
+/*constant value*/
+9
+4.5
+"string"
+'c'
+myname
+14name
+my_name
+### na$me namee$ na14me 
+na_me name_
+(){}[];:,5$ @ name _name 14name name14 \n 
+$name name_14 /**/ /*zz*/'' 'a' 'aaa' ' ' " " "a" 930232 
+34.34 0.3.0 2.2323.232 87634 345.4 = += -=  
+*=  /=  %=  //= ++ -- + - * / % ** //  == != > y ni no < >= <= bool 
+cuer dec descanso doble ent escan flot hacer 
+impri mas mientras ni no perso por retorno sequir si 
+vacio vamos y falso verdad booleano decontra personaje entero escanear flotar cuerda imprimir
+9
+4.5
+"string"
+'c'
 myname
 14name
 my_name
 
+myname
+14name
+my_name
 3.14.14
 'cat'
-
+domskrt
+-8
+tite
 /*Identifier*/
 dom
 domskrt
@@ -13,7 +98,7 @@ name14
 
 /*Assignment Operators*/
 = += -=  *=  /=  %=  //=
- 
+
 /*Unary Operators*/
 ++ -- 
 
@@ -51,17 +136,3 @@ _num
 4.5
 "string"
 'c'
-
-myname
-14name
-my_name
-
-
-
-
-
-
-
-
-
-
